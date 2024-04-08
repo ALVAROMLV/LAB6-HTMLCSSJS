68a18a4d0dea1613a0b082b1c2eb2ae078df47a1
alvarolopezv <alvaromlopezvargas@gmail.com>
2024-04-07 23:52:36 -0400

    DOM

---
+++ src/hw03/q03.js
@@ -0,0 +1,16 @@
+
+var enlaceHTML = '<a href="https://keybr.com">Google</a>';
+
+document.getElementById("enlaces-container").insertAdjacentHTML('beforeend', enlaceHTML);
+
+const buyList = [
+    "books",
+    "bread",
+    "eraser",
+    "earphones",
+    "collection-cards",
+    "hdmi",
+    "vga",
+    "motherboard",
+    "university-books",
+  ];
